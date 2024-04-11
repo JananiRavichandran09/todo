be731360d459e4e928158a506e876dea8e2b38f8
JananiRavichandran09 <jananiravichandran2001@gmail.com>
2024-04-11 16:41:48 +0530

    done

--- src/Component/Input.jsx
+++ src/Component/Input.jsx
@@ -13,87 +13,89 @@ const Input = ({ todoValue, setTodoValue, addTodo }) => {
     setDueDate("");
   };
   return (
-    <div class="container">
+    <div className="container">
       <h1
-        class="text-center m-3"
+        className="text-center m-3"
         style={{ color: "#198754", textAlign: "center" }}
       >
         My Todo
       </h1>
-      <div
-        className="row  justify-content-around text"
-        style={{ marginTop: "100px", textAlign: "center" }}
-      >
-        <input
-          style={{
-            padding: "10px",
-            marginRight: "100px",
-            borderRadius: "10px",
-          }}
-          class="col-lg-4 col-sm-12 p-2 m-1 border-success border-3"
-          type="text"
-          placeholder="Todo Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required
-        />
-        <input
-          style={{
-            padding: "10px",
-            marginRight: "100px",
-            borderRadius: "10px",
-          }}
-          class="col-lg-4 col-sm-12 p-2 m-1 border-success border-3"
-          type="text"
-          placeholder="Todo Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          required
-        />
-        <input
-          style={{
-            padding: "10px",
-            marginRight: "100px",
-            borderRadius: "10px",
-          }}
-          class="col-lg-4 col-sm-12 p-2 m-1 border-success border-3"
-          type="number"
-          placeholder="Priority"
-          value={priority}
-          onChange={(e) => setPriority(e.target.value)}
-          required
-        />
-        <input
-          style={{
-            padding: "10px",
-            marginRight: "100px",
-            borderRadius: "10px",
-          }}
-          class="col-lg-4 col-sm-12 p-2 m-1 border-success border-3"
-          type="date"
-          placeholder="dd-mm-yyyy"
-          value={dueDate}
-          onChange={(e) => setDueDate(e.target.value)}
-          required
-        />
-        <div>
-          <button
+      <div className="container">
+        <div
+          className="row  justify-content-around text"
+          style={{ marginTop: "100px", textAlign: "center" }}
+        >
+          <input
             style={{
               padding: "10px",
               marginRight: "100px",
-              borderRadius: "15px",
-              backgroundColor: "#198754",
-              border: "none",
               borderRadius: "10px",
-              color: "white",
             }}
-            class="col-lg-1 col-sm-3 col-3 p-2 m-1 btn btn-success border-success border-3"
-            onClick={() => {
-              addButtonCall(title, description, priority, dueDate);
+            class="col-lg-4 col-sm-12 p-2 m-1 border-success border-3"
+            type="text"
+            placeholder="Todo Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+          <input
+            style={{
+              padding: "10px",
+              marginRight: "100px",
+              borderRadius: "10px",
             }}
-          >
-            Add Todo
-          </button>
+            class="col-lg-4 col-sm-12 p-2 m-1 border-success border-3"
+            type="text"
+            placeholder="Todo Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+          <input
+            style={{
+              padding: "10px",
+              marginRight: "100px",
+              borderRadius: "10px",
+            }}
+            class="col-lg-4 col-sm-12 p-2 m-1 border-success border-3"
+            type="number"
+            placeholder="Priority"
+            value={priority}
+            onChange={(e) => setPriority(e.target.value)}
+            required
+          />
+          <input
+            style={{
+              padding: "10px",
+              marginRight: "100px",
+              borderRadius: "10px",
+            }}
+            class="col-lg-4 col-sm-12 p-2 m-1 border-success border-3"
+            type="date"
+            placeholder="dd-mm-yyyy"
+            value={dueDate}
+            onChange={(e) => setDueDate(e.target.value)}
+            required
+          />
+          <div>
+            <button
+              style={{
+                padding: "10px",
+                marginRight: "100px",
+                borderRadius: "15px",
+                backgroundColor: "#198754",
+                border: "none",
+                borderRadius: "10px",
+                color: "white",
+              }}
+              class="col-lg-1 col-sm-3 col-3 p-2 m-1 btn btn-success border-success border-3"
+              onClick={() => {
+                addButtonCall(title, description, priority, dueDate);
+              }}
+            >
+              Add Todo
+            </button>
+          </div>
         </div>
       </div>
     </div>
